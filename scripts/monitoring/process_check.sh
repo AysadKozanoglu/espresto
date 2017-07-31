69e8cb3108cb72f39f3092f5e4655d044618ab14
Aysad Kozanoglu <aysadx@gmail.com>
2017-07-31 13:17:01 +0200

    Update process_check.sh

--- scripts/monitoring/process_check.sh
+++ scripts/monitoring/process_check.sh
@@ -16,7 +16,10 @@
 
 # Validate number of arguments
 
-sleeping=10
+##sleeping=10
+host=$(cat /etc/hostname)
+emailTo="aysadx@gmail.com"
+logfile=/var/log/process.log
 
 if [ $# -ne 1 ] ; then
   echo "${DATE} SUNCERTAIN: Invalid number of arguments  Usage: process_running "
@@ -24,7 +27,7 @@ if [ $# -ne 1 ] ; then
   exit 1
 fi
 
-while true; do
+##while true; do
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 
@@ -32,13 +35,18 @@ DATE=`date +%Y-%m-%d:%H:%M:%S`
 if (( $(ps -ef | grep -v grep | grep $1 | wc -l) > 2 )) 
 then
   echo "${DATE} SUCCESS: Process '$1' is running "
-  echo "${DATE} SUCCESS: Process '$1' is running " >> /var/log/logtool.log
+  echo "${DATE} SUCCESS: Process '$1' is running " >> $logfile
 else
-  echo "${DATE} SERROR: Process '$1' is not running"
-  echo "${DATE} SERROR: Process '$1' is not running" >> /var/log/logtool.log
+  echo "${DATE} ERROR: Process '$1' is not running" >> $logfile
+  /etc/init.d/${1} start >> $logfile
+
+  echo "${DATE} ERROR: Process '$1' is not running. trying to restart"
+  sublogReport=$(head -n-5  $logfile)
+
+## mail -s "${DATE} ${host} $1 wurde neugestartet" $emailTo <<<"$host -> $1 wurde automatisch neu gestartet \n\n ${sublogReport}"
 fi
 
-sleep $sleeping
-done
+##sleep $sleeping
+##done
 
 exit 0
